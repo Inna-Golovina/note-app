8028873958fa665da4ae087232b02642d2f4cd94
Inna Golovina <igigig978@gmail.com>
2021-05-28 15:51:23 +0300

    Updates

---
+++ precache-manifest.523bad15c3e8f8d817340bf2ab77223d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0050302539200c504a8e104601de2847",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "4bc4a4804e35cd5f6c44",
+    "url": "/note-app/static/css/main.d05e2af9.chunk.css"
+  },
+  {
+    "revision": "5cdb69ba0dee736a1dea",
+    "url": "/note-app/static/js/2.91ac517b.chunk.js"
+  },
+  {
+    "revision": "4bc4a4804e35cd5f6c44",
+    "url": "/note-app/static/js/main.6857b8d9.chunk.js"
+  },
+  {
+    "revision": "7b8957cce76bc9748ed2",
+    "url": "/note-app/static/js/runtime~main.90d25505.js"
+  }
+]);
